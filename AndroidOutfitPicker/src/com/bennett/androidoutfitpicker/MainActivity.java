453caadb47db336c61551b11068d693697f8a621
Liz Bennett <liz@bennettworld.org>
2013-06-26 01:40:10 -0400

    Got tabs up and working. Created fragments for each tab. Adding placeholder icons.

--- AndroidOutfitPicker/src/com/bennett/androidoutfitpicker/MainActivity.java
+++ AndroidOutfitPicker/src/com/bennett/androidoutfitpicker/MainActivity.java
@@ -5,15 +5,37 @@ import android.app.Activity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.support.v4.app.NavUtils;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTabHost;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
 
 	//checking to see if git is working
+	private FragmentTabHost mTabHost;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        mTabHost = (FragmentTabHost)findViewById(android.R.id.tabhost);
+        mTabHost.setup(this, getSupportFragmentManager(), R.id.realtabcontent);
+
+        mTabHost.addTab(mTabHost.newTabSpec("outfits").setIndicator("",
+                getResources().getDrawable(R.drawable.ic_outfit_tab)),
+                OutfitFragment.class, null);
+        mTabHost.addTab(mTabHost.newTabSpec("wardrobe").setIndicator("",
+                getResources().getDrawable(R.drawable.ic_wardrobe_tab)),
+                WardrobeFragment.class, null);
+        mTabHost.addTab(mTabHost.newTabSpec("laundry").setIndicator("",
+                getResources().getDrawable(R.drawable.ic_laundry_tab)),
+                   LaundryFragment.class, null);
+        mTabHost.addTab(mTabHost.newTabSpec("fashion").setIndicator("",
+                getResources().getDrawable(R.drawable.ic_fashion_tab)),
+                FashionFragment.class, null);
+        
+        
+        
     }
 
     @Override
